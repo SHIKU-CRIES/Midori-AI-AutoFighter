060f6345cd242ac80ed6cdf721172dddcebbecc4
lunamidori5 <jaredteam@gmail.com>
2024-10-19 23:54:13 +0000

    lowered the base starts of foes

--- player.py
+++ player.py
@@ -539,8 +539,8 @@ class Player:
         self.level = level
         self.MHP: int = random.randint(2 * self.level, 250 * self.level) + 1000
         self.HP: int = self.MHP
-        self.Def: int = self.Def + int((self.MHP * (0.0005 * self.level)) * self.level) + 200
-        self.Atk: int = random.randint(115 * self.level, 250 * self.level) + (self.level * 125)
+        self.Def: int = self.Def + int((self.MHP * (0.000005 * self.level)) * self.level) + 200
+        self.Atk: int = random.randint(15 * self.level, 25 * self.level) + (self.level * 20)
         self.Regain: float = random.uniform(0.0001 * self.level, (self.level * 0.002)) + (self.level * 0.004)
         self.CritRate: float = random.uniform(0.001 * self.level, (self.level * 0.002)) + (self.level * 0.001)
         self.CritDamageMod: float = 2 + (self.level * 0.0025)
