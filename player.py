b842b138cefbe5a7c22b1710e57bf798840d14af
lunamidori5 <jaredteam@gmail.com>
2024-10-20 08:15:39 +0000

    lowered Luna's stats a bit more

--- player.py
+++ player.py
@@ -117,7 +117,7 @@ class Player:
 
     def check_name_mod(self):
         if themed_names[0] in self.PlayerName.lower():
-            dodge_buff = 0.045
+            dodge_buff = 0.025
             max_hp_debuff = random.randint(250, 2000)
             while self.MHP > max_hp_debuff:
                 dodge_buff = dodge_buff + 0.00001
