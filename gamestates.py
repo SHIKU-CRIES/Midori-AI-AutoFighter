cf8ec61f643195a57cf60deabdf6aeee89cd9a34
Luna Midori <jaredteam@gmail.com>
2024-10-31 23:18:54 -0700

    moved something

--- gamestates.py
+++ gamestates.py
@@ -422,9 +422,9 @@ def main(level):
                 
             if enrage_timer.timed_out:
                 fps_stat = font.render(f"FPS: {int(fps)}", True, (255, 255, 255))
-                enrage_timer_stat = font.render(f"Time of fight: {enrage_mod:1f}", True, (255, 255, 255))
-                fps_rect = fps_stat.get_rect(center=((SCREEN_WIDTH // 8) + 800, (SCREEN_HEIGHT // 2) - 400))
-                enrage_timer_rect = fps_stat.get_rect(center=((SCREEN_WIDTH // 8) + 400, (SCREEN_HEIGHT // 2) - 400))
+                enrage_timer_stat = font.render(f"Enrage: {enrage_mod:1f}", True, (255, 255, 255))
+                fps_rect = fps_stat.get_rect(center=((SCREEN_WIDTH // 8) + 600, (SCREEN_HEIGHT // 2) - 400))
+                enrage_timer_rect = fps_stat.get_rect(center=((SCREEN_WIDTH // 8) + 600, (SCREEN_HEIGHT // 2) - 600))
                 screen.blit(fps_stat, fps_rect)
                 screen.blit(enrage_timer_stat, enrage_timer_rect)
             else:
