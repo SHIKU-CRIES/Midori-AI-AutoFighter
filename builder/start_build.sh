fe558b69463da47e00f7bd53bca8a2756c837889
Luna Midori <jaredteam@gmail.com>
2024-11-02 20:40:33 -0700

    switch to root then back into userspace

--- builder/start_build.sh
+++ builder/start_build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build the Docker image
-docker build -t my-image .
+docker build -t game-builder .
 
 # Run the Docker container and wait until it finishes
-docker run --rm -v $(pwd):/game my-image
+docker run --rm -v $(pwd):/game game-builder
 
 # Remove the Docker image
-docker image rm my-image
+docker image rm game-builder
