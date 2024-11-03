c3c2804e94eb5d745ba3436e46021ec3e9983219
Luna Midori <jaredteam@gmail.com>
2024-11-02 21:52:16 -0700

    I think I fixed it?

--- builder/start_build.sh
+++ builder/start_build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+mkdir temp_game
+
+cp -t temp_game/. ../*
 
 # Build the Docker image
 docker build -t game-builder .
 
 # Run the Docker container and wait until it finishes
-docker run --rm -v $(pwd):/game game-builder
+docker run --rm -v $(pwd):/game -v $(pwd)/temp_game:/game-code game-builder
 
 # Remove the Docker image
 docker image rm game-builder
