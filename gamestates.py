d6996d8b8f8b37f64f1686e24969f5b4483bd89f
Luna Midori <jaredteam@gmail.com>
2024-11-03 01:20:09 -0800

    more testing

--- gamestates.py
+++ gamestates.py
@@ -35,7 +35,6 @@ SCREEN_WIDTH = 1600
 SCREEN_HEIGHT = 900
 
 photo_size = 128 * 2
-offset_photo_size = (photo_size + 200) * -1
 
 enrage_timer = timmer()
 temp_screen = Screen(SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -360,7 +359,7 @@ def main(level):
             # Draw the players profile picture
             player_profile_pic = pygame.image.load(os.path.join("photos", player.photo))
             player_profile_pic = pygame.transform.scale(player_profile_pic, (photo_size, photo_size))
-            screen.blit(player_profile_pic, (player_hp_bar.x, player_hp_bar.y + offset_photo_size))
+            screen.blit(player_profile_pic, ((SCREEN_WIDTH // 6) - player_offset, (SCREEN_HEIGHT // 2) + 160))
             
             stat_data = [
                 ("Stats of:", player.PlayerName),
@@ -465,7 +464,7 @@ def main(level):
             foe_profile_pic = pygame.image.load(os.path.join("photos", foe.photo))
             foe_profile_pic = pygame.transform.flip(foe_profile_pic, True, False)
             foe_profile_pic = pygame.transform.scale(foe_profile_pic, (photo_size, photo_size))
-            screen.blit(foe_profile_pic, (foe_hp_bar.x + 200, foe_hp_bar.y + offset_photo_size))
+            screen.blit(foe_profile_pic, (SCREEN_WIDTH * 5 // 6, (SCREEN_HEIGHT // 2) + 160))
 
             # Draw the current tossed items
             if player_item_index < len(player.Inv):
