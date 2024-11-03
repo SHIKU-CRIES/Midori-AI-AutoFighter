e102cf58628d068133ff4d5bdf5c758e2e179e7b
Luna Midori <jaredteam@gmail.com>
2024-11-02 22:39:20 -0700

    more build stuff

--- builder/start_build.sh
+++ builder/start_build.sh
@@ -18,7 +18,8 @@ docker run --rm -v $(pwd):/game -v $(pwd)/temp_game:/game-code game-builder
 # Remove the Docker image
 docker image rm game-builder
 
-rm -rf temp_game
-
 butler push output/windows lunamidori/midori-endless-auto-fighter:windows
 butler push output/linux lunamidori/midori-endless-auto-fighter:linux
+
+rm -rf output
+rm -rf temp_game
