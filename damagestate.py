bddad7d2a26135b05c5d56e114c4340b08ab5737
Luna Midori <jaredteam@gmail.com>
2024-11-05 06:25:17 -0800

    bug fixes

--- damagestate.py
+++ damagestate.py
@@ -11,7 +11,12 @@ white = Fore.WHITE
 
 def log(color, text):
     print(color + text + Style.RESET_ALL)
+    debug_log(text)
     return text
+
+def debug_log(text):
+    with open("debug_log.txt", "a") as f:
+        f.write(text)
 
 def take_damage(source: Player, target: Player, fight_env_list: list):
     """
@@ -33,26 +38,30 @@ def take_damage(source: Player, target: Player, fight_env_list: list):
         text_to_log = log(green, f"{target.PlayerName} dodged!")
     else:
         damage_dealt = ((current_item.damage * source.Atk) / 2) * (source.Vitality ** 3)
+        source_vit = (source.Vitality ** 3)
+        target_vit = (target.Vitality ** 5)
+        def_val = (target.Def / def_mod)
+        text_to_log = log(white, f"pre mitigated dmg: {damage_dealt}, target Vit: {target_vit}, source Vit: {source_vit}, target def: {def_val}")
 
         if source.CritRate >= random.random():
             mited_damage_dealt = (((damage_dealt * enrage_buff) / ((target.Def / def_mod) * (target.Vitality ** 5))) * source.CritDamageMod) * max(1, source.CritRate)
             mited_damage_dealt = mited_damage_dealt * random.uniform(0.95, 1.05)
 
             if enrage_timer.timed_out:
-                text_to_log = log(blue, f"Crit! {source.PlayerName} {current_item.game_obj} crits {target.PlayerName} for {mited_damage_dealt:.2f} damage! Enraged")
+                text_to_log += log(blue, f"Crit! {source.PlayerName} {current_item.game_obj} crits {target.PlayerName} for {mited_damage_dealt:.2f} damage! Enraged")
                 target.HP = target.HP - mited_damage_dealt
             else:
-                text_to_log = log(blue, f"Crit! {source.PlayerName} {current_item.game_obj} crits {target.PlayerName} for {mited_damage_dealt:.2f} damage!")
+                text_to_log += log(blue, f"Crit! {source.PlayerName} {current_item.game_obj} crits {target.PlayerName} for {mited_damage_dealt:.2f} damage!")
                 target.HP = target.HP - mited_damage_dealt
         else:
             mited_damage_dealt = ((damage_dealt * enrage_buff) / ((target.Def / def_mod) * (target.Vitality ** 5)))
             mited_damage_dealt = mited_damage_dealt * random.uniform(0.95, 1.05)
             
             if enrage_timer.timed_out:
-                text_to_log = log(red, f"Hit! {source.PlayerName} {current_item.game_obj} hits {target.PlayerName} for {mited_damage_dealt:.2f} damage! Enraged")
+                text_to_log += log(red, f"Hit! {source.PlayerName} {current_item.game_obj} hits {target.PlayerName} for {mited_damage_dealt:.2f} damage! Enraged")
                 target.HP = target.HP - mited_damage_dealt
             else:
-                text_to_log = log(red, f"Hit! {source.PlayerName} {current_item.game_obj} hits {target.PlayerName} for {mited_damage_dealt:.2f} damage!")
+                text_to_log += log(red, f"Hit! {source.PlayerName} {current_item.game_obj} hits {target.PlayerName} for {mited_damage_dealt:.2f} damage!")
                 target.HP = target.HP - mited_damage_dealt
         
         target.DamageTaken += mited_damage_dealt
