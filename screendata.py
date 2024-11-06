e1cccdf1eb94505c9e156edecdebe173d06fe037
Luna Midori <jaredteam@gmail.com>
2024-11-05 22:20:18 -0800

    added better lookingi stuff?

--- screendata.py
+++ screendata.py
@@ -3,4 +3,4 @@ import pygame
 
 class Screen:
     def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT):
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.SRCALPHA)
