3b855e21b5ee9c09288d03953004cba0425ca2d2
Luna Midori <jaredteam@gmail.com>
2024-11-10 06:48:40 -0800

    hopefully fixed str(self.type)

--- items.py
+++ items.py
@@ -20,24 +20,24 @@ class ItemType():
 
     def on_damage_taken(self, pre_damage_taken: float):
         """This function is called when the player takes damage.
-        If the item type is "damage", this function will handle the damage-related functionality."""
-        if self.type == "damage":
-            return float(pre_damage_taken / self.power)
+        If the item type is "defense", this function will handle the damage-related functionality."""
+        if "defense" in str(self.type):
+            return float((pre_damage_taken / self.power) / self.power)
         else:
             return float(pre_damage_taken / self.power)
 
     def on_damage_dealt(self, damage_delt: float):
         """This function is called when the player deals damage.
         If the item type is "damage", this function will handle the damage-related functionality."""
-        if self.type == "damage":
-            return float(damage_delt * self.power)
+        if "damage" in str(self.type):
+            return float((damage_delt * self.power) * self.power)
         else:
             return float(damage_delt * self.power)
 
     def stat_gain(self, power: float):
         """This function is called when the player gains stats.
         If the item type is "utility", this function will handle the dodge-related functionality."""
-        if self.type == "utility":
-            return float(power * self.power)
+        if "utility" in str(self.type):
+            return float((power * self.power) * self.power)
         else:
             return float(power * self.power)
