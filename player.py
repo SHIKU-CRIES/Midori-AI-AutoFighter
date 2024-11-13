a5e1b0723d0f3e2f3a2b6efda42ad4855a1c6dda
Luna Midori <jaredteam@gmail.com>
2024-11-13 03:15:25 -0800

    buffed players vit

--- player.py
+++ player.py
@@ -112,7 +112,7 @@ class Player:
                         os.remove(filename)
 
                     elif past_life_data['Vitality'] > 1.0000001:
-                        self.Vitality = self.Vitality + min(((past_life_data['Vitality'] - 1) / ((self.Vitality ** 15) + 100)), 0.01)
+                        self.Vitality = self.Vitality + min(((past_life_data['Vitality'] - 1) / ((self.Vitality ** 2) + 100)), 0.01)
                     
                     self.check_stats()
 
