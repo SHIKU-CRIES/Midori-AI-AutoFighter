dc7c141b4870411e79c7e5340aa6dfe65014e3e3
Luna Midori <jaredteam@gmail.com>
2024-11-15 00:47:48 -0800

    lowered it a bit

--- player.py
+++ player.py
@@ -112,7 +112,7 @@ class Player:
                         os.remove(filename)
 
                     elif past_life_data['Vitality'] > 1.0000001:
-                        self.Vitality = self.Vitality + max(((past_life_data['Vitality'] - 1) / ((self.Vitality ** 2))), 0.01) + 0.001
+                        self.Vitality = self.Vitality + max(((past_life_data['Vitality'] - 1) / ((self.Vitality ** 2))), 0.001) + 0.0001
                     
                     self.check_stats()
 
