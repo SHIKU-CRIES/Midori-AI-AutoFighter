121c2bc9484aaf3aef0b04dc9de43957f1e24e88
Luna Midori <jaredteam@gmail.com>
2024-11-16 12:02:55 -0800

    slow down enrage after last fix

--- gamestates.py
+++ gamestates.py
@@ -281,10 +281,7 @@ def main(level):
             foe_base_enrage_mod = (enrage_mod * (level_base_enrage_mod * foe.Vitality))
             bleed_mod = (0.00000001 * (player_base_enrage_mod * foe_base_enrage_mod)) + 1
 
-            if bleed_mod > 10:
-                def_mod = max(1, ((bleed_mod * 0.3) * max((bleed_mod * 0.05), 1.2)))
-            else:
-                def_mod = max(1, (bleed_mod * 0.2))
+            def_mod = max(1, (bleed_mod * 0.2))
 
             fps_cap = 20
             dt = clock.tick(fps_cap) / 1000
