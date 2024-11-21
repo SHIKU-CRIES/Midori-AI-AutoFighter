4cebbdea32817031ed68a254156675274c1bdcde
Luna Midori <jaredteam@gmail.com>
2024-11-21 10:10:55 -0800

    more bug fixes

--- damagestate.py
+++ damagestate.py
@@ -89,7 +89,7 @@ def take_damage(source: Player, target: Player, fight_env_list: list, def_mod: f
                 text_to_log = log(red, f"Hit! {source.PlayerName} {current_item.game_obj} hits {target.PlayerName} for {mited_damage_dealt:.2f} damage!")
 
         if mited_damage_dealt > target.HP:
-            mited_damage_dealt = target.HP + 10
+            mited_damage_dealt = target.HP + 1000
 
         target.HP -= int(max(mited_damage_dealt, 1))
     
