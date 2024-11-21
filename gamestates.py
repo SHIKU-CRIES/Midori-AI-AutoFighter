d0b166024d3e28115fe33d18056235fdb204c69f
Luna Midori <jaredteam@gmail.com>
2024-11-20 16:32:09 -0800

    speeds up even more

--- gamestates.py
+++ gamestates.py
@@ -280,8 +280,11 @@ def main(level):
             player_base_enrage_mod = (enrage_mod * (level_base_enrage_mod * player.Vitality))
             foe_base_enrage_mod = (enrage_mod * (level_base_enrage_mod * foe.Vitality))
 
-            if enrage_mod > 35:
-                buffed_starter = ((enrage_mod - 35) * 0.000000002)
+            if enrage_mod > 25:
+                buffed_starter = ((enrage_mod - 25) * 0.000000004)
+                bleed_mod = ((0.00000002 + buffed_starter) * (player_base_enrage_mod * foe_base_enrage_mod)) + 1
+            elif enrage_mod > 10:
+                buffed_starter = ((enrage_mod - 10) * 0.000000002)
                 bleed_mod = ((0.00000002 + buffed_starter) * (player_base_enrage_mod * foe_base_enrage_mod)) + 1
             else:
                 bleed_mod = (0.00000002 * (player_base_enrage_mod * foe_base_enrage_mod)) + 1
