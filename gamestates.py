cf8447a8763bd205da3761dc188d45edf1572a96
Luna Midori <jaredteam@gmail.com>
2024-11-21 11:40:58 -0800

    fixed a enrage bug

--- gamestates.py
+++ gamestates.py
@@ -280,11 +280,11 @@ def main(level):
             player_base_enrage_mod = (enrage_mod * (level_base_enrage_mod * player.Vitality))
             foe_base_enrage_mod = (enrage_mod * (level_base_enrage_mod * foe.Vitality))
 
-            if enrage_mod > 25:
-                buffed_starter = ((enrage_mod - 25) * 0.000000004)
+            if enrage_mod > 10:
+                buffed_starter = ((enrage_mod - 10) * 0.000000004) + ((enrage_mod - 5) * 0.000000002)
                 bleed_mod = ((0.00000002 + buffed_starter) * (player_base_enrage_mod * foe_base_enrage_mod)) + 1
-            elif enrage_mod > 10:
-                buffed_starter = ((enrage_mod - 10) * 0.000000002)
+            elif enrage_mod > 5:
+                buffed_starter = ((enrage_mod - 5) * 0.000000002)
                 bleed_mod = ((0.00000002 + buffed_starter) * (player_base_enrage_mod * foe_base_enrage_mod)) + 1
             else:
                 bleed_mod = (0.00000002 * (player_base_enrage_mod * foe_base_enrage_mod)) + 1
