159cba6c9acbb44aab83635bdde27f7b0879743d
Luna Midori <jaredteam@gmail.com>
2024-11-29 19:05:18 -0800

    lower items more to fix bug

--- items.py
+++ items.py
@@ -13,7 +13,7 @@ class ItemType():
 
     def upgrade(self, mod_fixed):
         """Upgrades the item's power stat."""
-        self.power += max(math.log10(random.uniform(0.1, 0.01) * self.check_mods(mod_fixed)), 0.00001)
+        self.power += max(math.log10(random.uniform(0.1, 0.01) * self.check_mods(mod_fixed / 10)), 0.00001)
     
     def check_mods(self, temp_power: float):
         for index, item_mod in enumerate(item_mods):
