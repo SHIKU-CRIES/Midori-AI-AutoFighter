3af83af91badb3f6f714b6223f1710c45ad92e9f
Luna Midori <jaredteam@gmail.com>
2024-12-01 00:19:56 -0800

    Updates to the way I start the game

--- play.sh
+++ play.sh
@@ -1,4 +1,15 @@
 git reset --hard HEAD
 git pull
-konsole -e "bash update.sh" &
-uv run main.py
+
+# Ask the user if they want to run the updater
+echo "Do you want to run the updater? (y/N)"
+read answer
+
+# If the user types "y", run the updater and then the game
+if [ "$answer" == "y" ]; then
+  konsole -e "bash update.sh" &
+  uv run main.py
+# If the user types anything else, just run the game
+else
+  uv run main.py
+fi
