bded6661d136e44dee98106b424e5a4bea050f24
Luna Midori <jaredteam@gmail.com>
2024-12-17 22:38:35 -0800

    bug fix?

--- gamestates.py
+++ gamestates.py
@@ -504,9 +504,9 @@ def main(level):
 
             # Draw the foe's profile picture
             if foe_hp_percent < 75:
-                foe.photodata.set_alpha(int(255 * foe_hp_percent / 75))
+                foe_photo_preloaded.set_alpha(int(255 * foe_hp_percent / 75))
 
-            screen.blit(foe.photodata, (foe_rect.x + 0, foe_rect.y + 85))
+            screen.blit(foe_photo_preloaded, (foe_rect.x + 0, foe_rect.y + 85))
 
             # Draw the current tossed items
             if player_item_index < len(player.Inv):
