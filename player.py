756a2a344a4ac12a2d36f9d15038afe620733441
Luna Midori <jaredteam@gmail.com>
2024-12-17 21:20:22 -0800

    .

--- player.py
+++ player.py
@@ -905,14 +905,14 @@ def render_player_obj(pygame, player: Player, player_profile_pic, screen, enrage
 
     # Draw player's HP bar
     player_hp_percent = player.HP / player.MHP * 100
-    player_hp_bar = pygame.Rect(player_rect.x, player_rect.y + 60, player_hp_percent * (width / 100), 5)
-    player_hp_bar_full = pygame.Rect(player_rect.x, player_rect.y + 60, width, 5)
+    player_hp_bar = pygame.Rect(player_rect.x, player_rect.y + 20, player_hp_percent * (width / 100), 5)
+    player_hp_bar_full = pygame.Rect(player_rect.x, player_rect.y + 20, width, 5)
     pygame.draw.rect(screen, (255, 0, 0), player_hp_bar_full)
     pygame.draw.rect(screen, (0, 255, 0), player_hp_bar)
 
     # Draw HP percentage
     player_hp_percent_text = font.render(f"{player_hp_percent:.2f}%", True, (255, 255, 255))
-    player_hp_percent_rect = player_hp_percent_text.get_rect(center=(x + 85, y))
+    player_hp_percent_rect = player_hp_percent_text.get_rect(center=(x + 120, y))
     screen.blit(player_hp_percent_text, player_hp_percent_rect)
 
     if player_hp_percent < 75:
