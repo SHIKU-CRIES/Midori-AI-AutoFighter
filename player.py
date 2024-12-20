d7e508a93c599ab781c8dd96a36ac54e952b38da
lunamidori5 <jaredteam@gmail.com>
2024-12-20 09:22:09 -0800

    lower regain

--- player.py
+++ player.py
@@ -785,7 +785,7 @@ class Player:
         hp_up: int = random.randint(400 * self.level, 550 * self.level * int_mod)
         def_up: int = random.randint(5 * self.level, 200 * self.level * int_mod)
         atk_up: int = random.randint(15 * self.level, 350 * self.level * int_mod)
-        regain_up: float = random.uniform(0.00001, 0.00005 * self.level)
+        regain_up: float = random.uniform(0.00000001, 0.0000005 * self.level)
         critrate_up: float = random.uniform(0.001 * self.level, 0.0025 * self.level) * max((mod_fixed / 10000), 1)
         critdamage_up: float = random.uniform(0.004 * self.level, 0.008 * self.level) * max((mod_fixed / 10000), 1)
         dodgeodds_up: float = random.uniform(0.000002 * self.level, 0.00004 * self.level) * max((mod_fixed / 10000), 1)
@@ -866,7 +866,7 @@ class Player:
         top_level_full = 5000
 
         self.level = level
-        self.MHP: int = random.randint(10 * self.level, 15 * self.level) + 1000
+        self.MHP: int = random.randint(10 * self.level, 25 * self.level) + 1000
         self.HP: int = self.MHP
         self.Def: int = self.Def + int(self.MHP * (0.000005 * self.level)) + 500
         self.Atk: int = random.randint(10 * self.level, 20 * self.level) + (self.level * 2)
