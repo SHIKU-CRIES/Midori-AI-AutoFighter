72db69e0f3120c98bd8d258d515168670f3272a4
lunamidori5 <jaredteam@gmail.com>
2024-12-20 00:48:54 -0800

    testing adding bleed to luna

--- damagestate.py
+++ damagestate.py
@@ -4,6 +4,8 @@ from player import Player
 
 from colorama import Fore, Style
 
+from themedstuff import themed_names
+
 red = Fore.RED
 green = Fore.GREEN
 blue = Fore.BLUE
@@ -18,6 +20,38 @@ def debug_log(text):
         f.write(f"\n{text}")
     
     return text
+
+def check_passive_mod(source: Player, target: Player, mited_damage_dealt: float):
+    if themed_names[0] in source.PlayerName.lower():
+        log(red, f"{source.PlayerName} adds bleed to {target.PlayerName} for {max(mited_damage_dealt / 100, 25):.2f}")
+        target.Bleed += max(mited_damage_dealt / 100, 25)
+
+    if themed_names[1] in source.PlayerName.lower():
+        pass
+        
+    if themed_names[2] in source.PlayerName.lower():
+        pass
+
+    if themed_names[3] in source.PlayerName.lower():
+        pass
+
+    if themed_names[4] in source.PlayerName.lower():
+        pass
+
+    if themed_names[5] in source.PlayerName.lower():
+        pass
+
+    if themed_names[6] in source.PlayerName.lower():
+        pass
+
+    if themed_names[7] in source.PlayerName.lower():
+        pass
+
+    if themed_names[8] in source.PlayerName.lower():
+        pass
+
+    if themed_names[9] in source.PlayerName.lower():
+        pass
 
 def apply_damage_item_effects(source: Player, target: Player, mited_damage_dealt: float):
         
@@ -58,31 +92,37 @@ def take_damage(source: Player, target: Player, fight_env_list: list, def_mod: f
     enrage_buff = fight_env_list[0]
     enrage_timer = fight_env_list[1]
 
+    mited_damage_dealt = float(0)
+    random_crit = random.random()
+    source_vit = max((source.Vitality / def_mod) ** 4, 0.005)
+    target_vit = max((target.Vitality / def_mod) ** 4, 0.005)
+    def_val = ((target.Def / def_mod) ** 2)
+    damage_dealt = ((1 * (source.Atk * source_vit)) * 2)
+    crit = False
+    # text_to_log = log(white, f"pre mitigated dmg: {damage_dealt}, target Vit: {target_vit}, source Vit: {source_vit}, target def: {def_val}")
+
+    if source.CritRate >= random_crit:
+        crit = True
+        damage_dealt = apply_damage_item_effects(source, target, damage_dealt * (enrage_buff * (source.CritDamageMod * max(1, source.CritRate)) * def_mod))
+        mited_damage_dealt = float(damage_dealt / max(def_val * target_vit, 2))
+        mited_damage_dealt = mited_damage_dealt * random.uniform(0.95, 1.05)
+    else:
+        damage_dealt = apply_damage_item_effects(source, target, damage_dealt * (enrage_buff * def_mod))
+        mited_damage_dealt = float(damage_dealt / max(def_val * target_vit, 2))
+        mited_damage_dealt = mited_damage_dealt * random.uniform(0.95, 1.05)
+    
+    check_passive_mod(source, target, mited_damage_dealt)
+
     if (target.DodgeOdds / enrage_buff) >= random.random():
         text_to_log = log(green, f"{target.PlayerName} dodged!")
     else:
-        mited_damage_dealt = float(0)
-        source_vit = max((source.Vitality / def_mod) ** 4, 0.005)
-        target_vit = max((target.Vitality / def_mod) ** 4, 0.005)
-        def_val = ((target.Def / def_mod) ** 2)
-        damage_dealt = ((1 * (source.Atk * source_vit)) * 2)
-        # text_to_log = log(white, f"pre mitigated dmg: {damage_dealt}, target Vit: {target_vit}, source Vit: {source_vit}, target def: {def_val}")
-
-        if source.CritRate >= random.random():
-            damage_dealt = apply_damage_item_effects(source, target, damage_dealt * (enrage_buff * (source.CritDamageMod * max(1, source.CritRate)) * def_mod))
-            mited_damage_dealt = float(damage_dealt / max(def_val * target_vit, 2))
-            mited_damage_dealt = mited_damage_dealt * random.uniform(0.95, 1.05)
-            
-
+        if crit:
             if enrage_timer.timed_out:
                 text_to_log = log(blue, f"Crit! {source.PlayerName} crits {target.PlayerName} for {mited_damage_dealt:.2f} damage! Enraged")
             else:
                 text_to_log = log(blue, f"Crit! {source.PlayerName} crits {target.PlayerName} for {mited_damage_dealt:.2f} damage!")
+        
         else:
-            damage_dealt = apply_damage_item_effects(source, target, damage_dealt * (enrage_buff * def_mod))
-            mited_damage_dealt = float(damage_dealt / max(def_val * target_vit, 2))
-            mited_damage_dealt = mited_damage_dealt * random.uniform(0.95, 1.05)
-            
             if enrage_timer.timed_out:
                 text_to_log = log(red, f"Hit! {source.PlayerName} hits {target.PlayerName} for {mited_damage_dealt:.2f} damage! Enraged")
             else:
@@ -92,6 +132,6 @@ def take_damage(source: Player, target: Player, fight_env_list: list, def_mod: f
             mited_damage_dealt = target.HP + 1000
 
         target.HP -= int(max(mited_damage_dealt, 1))
-    
+
         if enrage_buff > 10:
             target.Bleed += max(mited_damage_dealt / target.MHP, 0.01)
