2e6e45cba0ee1e27781b7d22a93b4ce06a43df57
Luna Midori <jaredteam@gmail.com>
2025-02-22 10:59:58 -0800

    fix: adjust top level values in player level settings

--- player.py
+++ player.py
@@ -880,8 +880,8 @@ class Player:
         self.check_stats()
     
     def set_level(self, level):
-        top_level = 100000
-        top_level_full = 50000
+        top_level = 500
+        top_level_full = 1000
 
         self.level = level
         self.MHP: int = random.randint(10 * self.level, 25 * self.level) + 1000
