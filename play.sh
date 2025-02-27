d912c2af9d67072dd4db9e1fb0884716eb5d3dd5
Luna Midori <jaredteam@gmail.com>
2025-02-27 00:34:28 -0800

    fix: simplify game startup script by removing updater prompt

--- play.sh
+++ play.sh
@@ -1,15 +1,4 @@
 git reset --hard HEAD
 git pull
 
-# Ask the user if they want to run the updater
-echo "Do you want to run the updater? (y/N)"
-read answer
-
-# If the user types "y", run the updater and then the game
-if [ "$answer" == "y" ]; then
-  konsole -e "bash update.sh" &
-  uv run main.py
-# If the user types anything else, just run the game
-else
-  uv run main.py
-fi
+uv run main.py
