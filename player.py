5b2c1ec362edccb85f49d1aba246167b77e363de
Luna Midori <jaredteam@gmail.com>
2025-02-27 01:15:26 -0800

    fix: correct dodge buff calculation for improved accuracy

--- player.py
+++ player.py
@@ -342,7 +342,7 @@ class Player:
             max_hp_debuff = self.MHP / 4
 
             while self.MHP > max_hp_debuff:
-                dodge_buff = dodge_buff + (0.0000000007 * self.Vitality)
+                dodge_buff = dodge_buff + (0.0000007 * self.Vitality)
                 self.MHP = self.MHP - 1
 
             self.Atk = int(self.Atk * 1)
