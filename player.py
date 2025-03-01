8603144f3e4dddc74454af8db79bbdb972e44411
Luna Midori <jaredteam@gmail.com>
2025-03-01 01:47:21 -0800

    fix: adjust stat modifier calculation for improved gameplay balance

--- player.py
+++ player.py
@@ -310,8 +310,8 @@ class Player:
 
         stat_modifiers = {}
 
-        for i in range(45):
-            new_key = (i * 0.25)
+        for i in range(65):
+            new_key = (i * 0.05)
             new_value = to_be_lowered_by ** max(0.7 * (i + 1), 1.0)
             stat_modifiers[new_key] = new_value
 
