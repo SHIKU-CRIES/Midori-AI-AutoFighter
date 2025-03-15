31331ff281eb3f33dfc101c22a8ebc1480f8b847
Luna Midori <jaredteam@gmail.com>
2025-03-15 11:30:44 -0700

    fix: adjust healing calculation to use a different exponent for regain formula

--- player.py
+++ player.py
@@ -878,7 +878,7 @@ class Player:
         return (damage_pre / (self.Mitigation * self.Vitality))
     
     def regain_hp(self):
-        self.heal_damage(min(self.MHP, self.HP + round((self.Regain * self.Vitality) ** 0.5)))
+        self.heal_damage(min(self.MHP, self.HP + (self.Regain * self.Vitality) ** 0.2))
 
     def take_dot(self):
         for dot in self.DOTS:
