2b843759f55bfda7b6d7273e2fd1512cb2b96e77
Luna Midori <jaredteam@gmail.com>
2025-03-16 09:37:55 -0700

    fix: update healing over time effect type and duration for players

--- damagestate.py
+++ damagestate.py
@@ -102,7 +102,7 @@ def check_passive_mod(foelist: list[Player], playerlist: list[Player], source: P
             if source.isplayer == player.isplayer:
                 if player.HP < source.HP:
                     player.heal_damage(player.HP * 0.05)
-                    player.HOTS.append(healingovertimetype("Heal", round(player.HP * 0.001), 2500, "generic", source.PlayerName, 2))
+                    player.HOTS.append(healingovertimetype("Heal", round(player.HP * 0.001), 2500, "light", source.PlayerName, 1))
 
     if themed_names[3] in source.PlayerName.lower():
         if target.MHP > source.MHP:
