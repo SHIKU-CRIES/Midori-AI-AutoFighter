f5e0ca766a034034c24bafeb6264fa682a4e360f
Luna Midori <jaredteam@gmail.com>
2025-03-18 12:51:13 -0700

    fix: comment out butler push commands in start_build script for debugging purposes

--- builder/start_build.sh
+++ builder/start_build.sh
@@ -16,8 +16,8 @@ docker run --rm -v $(pwd):/game -v $(pwd)/temp_game:/game-code game-builder
 # Remove the Docker image
 docker image rm game-builder
 
-butler push output/windows lunamidori/midori-endless-auto-fighter:windows
-butler push output/linux lunamidori/midori-endless-auto-fighter:linux
+#butler push output/windows lunamidori/midori-endless-auto-fighter:windows
+#butler push output/linux lunamidori/midori-endless-auto-fighter:linux
 
 sleep 600
 
