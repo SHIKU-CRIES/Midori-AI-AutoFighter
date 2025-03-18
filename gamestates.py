407d88b0a9260a868982fcec8616d5b2bf59acef
Luna Midori <jaredteam@gmail.com>
2025-03-17 23:42:53 -0700

    fix: update damage calculation to include target type for bleed damage

--- gamestates.py
+++ gamestates.py
@@ -318,7 +318,7 @@ def main(level):
 
                         if len(playerlist) > 0:
                             target_to_damage = random.choice(playerlist)
-                            target_to_damage.take_damage(take_damage(foelist, playerlist, target_to_damage, person, person.deal_damage(bleed_mod)))
+                            target_to_damage.take_damage(take_damage(foelist, playerlist, target_to_damage, person, person.deal_damage(bleed_mod, target_to_damage.Type)))
 
                             if target_to_damage.HP < 1:
                                 target_to_damage.save_past_life()
@@ -348,7 +348,7 @@ def main(level):
 
                         if len(foelist) > 0:
                             target_to_damage = random.choice(foelist)
-                            target_to_damage.take_damage(take_damage(foelist, playerlist, target_to_damage, person, person.deal_damage(bleed_mod)))
+                            target_to_damage.take_damage(take_damage(foelist, playerlist, target_to_damage, person, person.deal_damage(bleed_mod, target_to_damage.Type)))
 
                             if target_to_damage.HP < 1:
                                 foelist.remove(target_to_damage)
