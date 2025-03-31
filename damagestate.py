9e8c55b598c54e0e92743fcf11c81c4088e2ed31
Luna Midori <jaredteam@gmail.com>
2025-03-31 06:07:41 -0700

    fix: update healing over time effect to use source player's name and adjust healing amount

--- damagestate.py
+++ damagestate.py
@@ -179,7 +179,7 @@ def check_passive_mod(foelist: list[Player], playerlist: list[Player], source: P
                 if player.PlayerName is not source.PlayerName:
                     if player.HP < source.MHP * 0.24:
                         player.heal_damage(source.Atk * 0.005)
-                        player.gain_healing_over_time(healingovertimetype("Heal", round(player.MHP * 0.001), 250, player.Type, source.PlayerName, 1))
+                        player.gain_healing_over_time(healingovertimetype(f"{source.PlayerName}\'s Heal", round(player.MHP * 0.01), 5, player.Type, source.PlayerName, 1))
                     
                     if len(player.DOTS) > 0:
                         to_be_moved = random.choice(player.DOTS)
