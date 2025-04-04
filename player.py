5baab9f1724fdb388a30065b8f082a22073b8ef0
Luna Midori <jaredteam@gmail.com>
2025-04-04 10:59:53 -0700

    fix: correct damage handling in dot effect processing

--- player.py
+++ player.py
@@ -501,7 +501,7 @@ class Player:
                 dot_damage = dot.tick()
 
                 for i in range(dot.tick_interval):
-                    self.take_damage_nododge(1, self.Type.damage_mod(dot_damage, dot.damage_type))
+                    self.take_damage(1, self.Type.damage_mod(dot_damage, dot.damage_type))
                 
                 dot.damage = (dot.damage * 0.85)
             else:
