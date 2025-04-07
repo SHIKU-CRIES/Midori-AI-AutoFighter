1535208922bbf3d70a2dd96b4ee670d9753b16fd
Luna Midori <jaredteam@gmail.com>
2025-04-07 01:56:37 -0700

    refactor: adjust level base enrage modifier calculation for improved balance

--- gamestates.py
+++ gamestates.py
@@ -368,7 +368,7 @@ def main(level):
             fps = clock.get_fps()
 
             enrage_mod = enrage_timer.get_timeout_duration()
-            level_base_enrage_mod = (level / max(level / 5, 15))
+            level_base_enrage_mod = (level * max(level / 1000, 2))
             player_base_enrage_mod = (enrage_mod * level_base_enrage_mod)
             foe_base_enrage_mod = (enrage_mod * level_base_enrage_mod)
 
